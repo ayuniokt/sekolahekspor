20930f8a7188009097a4b70458491bc2f86a7f7c
19102193 <19102193@ittelkom-pwt.ac.id>
2022-03-01 20:41:45 +0700

    first commit

---
+++ latian.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html leng="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width-device-width, initial-scale-1.0">
+    <title>Belajar BE</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <?php
+        $varangka1 = "12";
+        $varangka2 = "4";
+        $varhasil = "16";
+    ?>
+    <?php echo "".$varangka1 ?>
+    <?php echo "+".$varangka2 ?>
+    <?php echo "=".$varhasil ?>
+</body>
+</html>
