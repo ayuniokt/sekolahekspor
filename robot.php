91d17a18d46635f5b5e30cc59521d5972f354d39
19102193 <19102193@ittelkom-pwt.ac.id>
2022-03-11 19:24:32 +0700

    commit: (tugasrobotopp)

---
+++ robot.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html leng="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width-device-width, initial-scale-1.0">
+    <title>AYUNI OKTAVIANINGRUM</title>
+</head>
+<body>
+    <h1>Latihan Robot</h1>
+    <?php
+class Robot
+{
+    private $warnaKepala, $warnaBadan, $warnaKaki, $jSenjata;
+    public function __construct($warnaKepala, $warnaBadan, $warnaKaki, $jSenjata)
+    {
+        $this->warnaKepala = $warnaKepala;
+        $this->warnaBadan = $warnaBadan;
+        $this->warnaKaki = $warnaKaki;
+        $this->jSenjata = $jSenjata;
+    }
+
+    public function getWarnaBadan()
+    {
+        echo $this->warnaKepala . " " . $this->warnaBadan . " " . $this->warnaKaki . " " . $this->jSenjata;
+    }
+    public function changeWarnaBadan($warnaBadan, $warnaKaki, $jSenjata)
+    {
+        $this->warnaBadan = $warnaBadan;
+        $this->warnaKaki = $warnaKaki;
+        $this->jSenjata = $jSenjata;
+    }
+}
+
+$robot = new Robot("Merah", "Biru", "Kuning", 3);
+$robot->getWarnaBadan();
+$robot->changeWarnaBadan("Hijau", "Putih", 2);
+echo "<br>";
+$robot->getWarnaBadan();
+    ?>
+</body>
+</html>
